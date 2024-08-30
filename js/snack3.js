88b717d642bd733b90b9a03a7785c159fd475bec
AndreaMargutti <marguttiandrea30@gmail.com>
2024-08-30 15:50:30 +0200

    created bikes

--- js/snack3.js
+++ js/snack3.js
@@ -6,3 +6,28 @@ Stampare in console la bici con peso minore utilizzando destructuring e template
 */
 
 // # Fase di preparazione
+//creo l'array di bici
+const bikes = [
+    {
+        name: 'fireKeeper',
+        weight: 10
+    },
+    {
+        name: 'storm',
+        weight: 4
+    },
+    {
+        name: 'wind surfer',
+        weight: 5
+    },
+    {
+        name: 'speedster',
+        weight: 3
+    },
+    {
+        name: 'Limbus 2000',
+        weight: 7
+    },
+];
+
+// TODO: cercare la bici con il peso minore
