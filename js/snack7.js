697edea259b2028682cd991dc8459a8beb4853ba
AndreaMargutti <marguttiandrea30@gmail.com>
2024-09-02 12:28:23 +0200

    snack7

---
+++ js/snack7.js
@@ -0,0 +1,49 @@
+// ! SNACK 7 (02/09/24)
+// TODO: Crea un array di oggetti che rappresentano delle persone. Ogni persona ha un nome, un cognome e un'età. Crea quindi un nuovo array inserendo, per ogni persona, una frase con il nome e cognome e l'indicazione se può guidare, in base all'età.
+
+// # Fase di preparazione
+//creo l'array
+const people = [
+    {
+        name: 'Andrea',
+        lastName: 'Margutti',
+        age: 24
+    },
+    {
+        name: 'Luca',
+        lastName: 'Tommasini',
+        age: 14
+    },
+    {
+        name: 'Giovanni',
+        lastName: 'Di Pasqua',
+        age: 36,
+    },
+    {
+        name: 'Alberto',
+        lastName: 'Framindola',
+        age: 18,
+    },
+    {
+        name: 'Piero',
+        lastName: 'Morizzani',
+        age: 6,
+    }
+];
+console.log(people);
+
+// # Fase di elaborazione
+//creo un array nuovo
+const driverLicense = people.map((person) => {
+    let string = `${person.name} ${person.lastName}, avendo ${person.age} anni `
+    if (person.age > 18) {
+        let isCapable = 'può guidare';
+        string += isCapable;
+    } else {
+        isCapable = 'non può guidare';
+        string += isCapable;
+    };
+    return string;
+});
+
+console.log(driverLicense);
